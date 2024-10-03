94ce1b6dd8e6fbd973fd1495defae3fdd31127d1
prohorp <usoltsev05@mail.ru>
2024-10-03 22:40:11 +0800

    hw04

--- hw04/main.cpp
+++ hw04/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <tuple>
+#include <array>
 #include <type_traits>
 
 // requires std::string<T>
@@ -20,8 +22,14 @@ void print_ip(T param)
 }
 
 // requires std::container<T>
+// Если у объекта есть метод emplace_back - значит это контейнер
+// std::string не имеет этого метода
 template <typename T,
-          std::enable_if_t<std::is_same_v<decltype(std::declval<T>().emplace_back()), decltype(std::declval<T>().emplace_back())>, bool> = true>
+          std::enable_if_t<
+              std::is_same_v<
+                  decltype(std::declval<T>().emplace_back()),
+                  decltype(std::declval<T>().emplace_back())>,
+              bool> = true>
 void print_ip(T param)
 {
     std::cout << "print_ip_Container:" << std::endl;
@@ -31,12 +39,45 @@ void print_ip(T param)
     }
 }
 
+// requires std::tuple<T>
+// получаем тип первого элемента и количество элементов в tuple(T)
+// создаем массив на основании этого
+// далее массив преобразуем в tuple через tuple_cat и сравниваем с типом T
+template <typename T,
+          std::enable_if_t<
+              std::is_same_v<T,
+                             decltype(std::tuple_cat(std::declval<
+                                                     std::array<
+                                                         std::remove_reference_t<decltype(std::get<0>(std::declval<T>()))>,
+                                                         std::tuple_size_v<std::remove_reference_t<decltype(std::declval<T>())>>>>()))>,
+              bool> = true>
+void print_ip(T param)
+{
+    std::cout << "print_ip_tuple:" << std::get<0>(param) << std::endl;
+}
+
 int main()
 {
     std::string str{"AAA"};
-    std::vector vec{1,2,3};
+    auto tp = std::make_tuple(123, 456, 789, 0);
+    std::array<std::remove_reference_t<decltype(std::get<0>(tp))>, std::tuple_size_v<decltype(tp)>> arr;
+    auto tp_1 = std::tuple_cat(arr);
+    // Проверка типов
+    if constexpr (std::is_same_v<decltype(tp), decltype(tp_1)>)
+    {
+        std::cout << "Типы одинаковы\n";
+    }
+    else
+    {
+        std::cout << "Типы различны\n";
+    }
+
+    std::cout << std::get<0>(tp_1) << "." << std::get<1>(tp_1) << "." << std::get<2>(tp_1) << "." << std::get<0>(tp_1) << std::endl;
+
+    std::vector vec{1, 2, 3};
 
     print_ip(555);
     print_ip(str);
     print_ip(vec);
+    print_ip(tp);
 }
