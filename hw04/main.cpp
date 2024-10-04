4dd1399e677b31f8dfffc526d0d9aa6bce3ccab9
prohorp <usoltsev05@mail.ru>
2024-10-04 09:26:44 +0800

    hw04

--- hw04/main.cpp
+++ hw04/main.cpp
@@ -8,9 +8,10 @@
 // requires std::string<T>
 template <typename T,
           std::enable_if_t<std::is_same_v<T, std::string>, bool> = true>
-void print_ip(T param)
+void print_ip(T string)
 {
-    std::cout << "print_ip_String:" << param << std::endl;
+    std::cout << "print_ip_String:";
+    std::cout << string << std::endl;
 }
 
 // requires std::integral<T>
@@ -18,7 +19,24 @@ template <typename T,
           std::enable_if_t<std::is_integral_v<T>, bool> = true>
 void print_ip(T param)
 {
-    std::cout << "print_ip_integer:" << param << std::endl;
+    //P.S. я в курсе про разный порядок байт на разных архитектурах
+    //в качестве простого примера это работает на x86
+    union
+    {
+        T valueIntegral;
+        char valueChar[sizeof(T)];
+    } u;
+    u.valueIntegral = param;
+    std::cout << "print_ip_integer:";
+    for (ssize_t i = sizeof(u.valueChar)-1; i >= 0; i--)
+    {
+        std::size_t tmp = static_cast<uint8_t>(u.valueChar[i]);
+        if (i == sizeof(u.valueChar)-1)
+            std::cout << tmp;
+        else
+            std::cout << '.' << tmp;
+    }
+    std::cout << std::endl;
 }
 
 // requires std::container<T>
@@ -32,7 +50,8 @@ template <typename T,
               bool> = true>
 void print_ip(T param)
 {
-    std::cout << "print_ip_Container:" << std::endl;
+    std::cout << "print_ip_Container:";
+    std::cout << std::endl;
     for (auto &i : param)
     {
         std::cout << i << std::endl;
@@ -53,7 +72,8 @@ template <typename T,
               bool> = true>
 void print_ip(T param)
 {
-    std::cout << "print_ip_tuple:" << std::get<0>(param) << std::endl;
+    std::cout << "print_ip_tuple:";
+    std::cout << std::get<0>(param) << std::endl;
 }
 
 int main()
@@ -76,7 +96,7 @@ int main()
 
     std::vector vec{1, 2, 3};
 
-    print_ip(555);
+    print_ip( int32_t{2130706433});
     print_ip(str);
     print_ip(vec);
     print_ip(tp);
