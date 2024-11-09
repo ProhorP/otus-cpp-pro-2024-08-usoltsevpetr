34d6b8c7187f5fc2973443f5935b2e644645968c
prohorp <usoltsev05@mail.ru>
2024-11-09 23:23:55 +0800

    hw06 итератор

--- hw06/main.cpp
+++ hw06/main.cpp
@@ -11,7 +11,7 @@ private:
     int level;
     int data;
     std::list<Matrix> child;
-    /* data */
+
 public:
     Matrix() : root(true) {};
     Matrix(std::array<int, 2> _addr, int level, int index);
@@ -23,15 +23,96 @@ public:
         else
             return child.emplace_back(addr, level + 1, index);
     }
+    void print() const
+    {
+        std::cout << "addr";
+        for (size_t i = 0; i < addr.size(); i++)
+            std::cout << "[" << addr[i] << "]";
+        std::cout << " data=" << data << std::endl;
+    }
     Matrix &operator=(int val)
     {
         data = val;
 
-        std::cout << "addr";
-        for (size_t i = 0; i < addr.size(); i++)
-            std::cout << "[" << addr[i] << "]";
-        std::cout << " operator=" << val << std::endl;
+        print();
         return *this;
+    }
+
+    struct iterator
+    {
+    public:
+        explicit iterator(Matrix *ptr) : ptr{ptr}, level{0}
+        {
+            if (ptr)
+            {
+                iters[level].first = ptr->child.begin();
+                iters[level].second = ptr->child.end();
+                rewind();
+            }
+        };
+        iterator &operator++()
+        {
+
+            // на самом глубоком уровне переключаемся на след элемент списка
+            // если мы достигли конца списка, то переходим на уровень выше и повторяем снова
+            while (++iters[level].first == iters[level].second)
+                if (--level < 0)
+                {
+                    // если выше нет итераторов - зануляем текущий указатель на матрицу
+                    // что будет значить конец итератора в рамках всей матрицы
+                    ptr = nullptr;
+                    return *this;
+                }
+
+            // Если мы переключались на уровни выше, то находим первый элемент от текущего уровня
+            if (level != 2 - 1)
+                rewind();
+
+            return *this;
+        }
+        void rewind()
+        {
+            while (iters[level].first->child.begin() != iters[level].first->child.end())
+            {
+                iters[level + 1].first = iters[level].first->child.begin();
+                iters[level + 1].second = iters[level].first->child.end();
+                level++;
+            }
+        }
+        bool operator!=(const iterator &other)
+        {
+            return ptr != other.ptr;
+        }
+
+        Matrix &operator*()
+        {
+            return get();
+        }
+
+        Matrix &get()
+        {
+            if (level == 2 - 1)
+                return *iters[level].first;
+            else
+                throw "нарушена глубина матрицы";
+        }
+
+    private:
+        Matrix *ptr;
+        //массив пар итераторов(текущий итератор и конец)
+        //размер массива = N-мерности матрицы
+        std::array<std::pair<std::list<Matrix>::iterator,std::list<Matrix>::iterator>, 2> iters;
+        int level;
+    };
+
+    iterator begin()
+    {
+        return iterator(this);
+    }
+
+    iterator end() const
+    {
+        return iterator(nullptr);
     }
 };
 
@@ -50,4 +131,10 @@ int main()
 
     Matrix m;
     m[4][2] = 42;
+    m[5][7] = 57;
+
+    for (auto &tmp : m)
+    {
+        tmp.print();
+    }
 }
